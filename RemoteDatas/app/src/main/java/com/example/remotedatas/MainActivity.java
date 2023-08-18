e2a68509aa817c1ce7c8098a2da95c05daa0551c
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-08-18 13:48:49 +0545

    remote server

--- RemoteDatas/app/src/main/java/com/example/remotedatas/MainActivity.java
+++ RemoteDatas/app/src/main/java/com/example/remotedatas/MainActivity.java
@@ -1,72 +1,82 @@
 package com.example.remotedatas;
 
 
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    EditText User , Pass;
+    Button button;
+    EditText Name, Email;
+    String server_url = "http://192.168.1.121/app/DBConfig.php"; // Remove the space in the URL
     AlertDialog.Builder builder;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.btn);
-        User = (EditText) findViewById(R.id.username);
-        Pass = (EditText) findViewById(R.id.password);
+        button = findViewById(R.id.button);
+        Name = findViewById(R.id.name1);
+        Email = findViewById(R.id.email1);
         builder = new AlertDialog.Builder(MainActivity.this);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "http://192.168.1.77//MobileProgramming/user_data.php";
-                RequestQueue req = Volley.newRequestQueue(MainActivity.this);
-                StringRequest strReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                final String name, email;
+                name = Name.getText().toString();
+                email = Email.getText().toString();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                        builder.setTitle("Server Response");
+                        builder.setMessage("Response: " + response);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Name.setText("");
+                                Email.setText("");
+                            }
+                        });
+
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage().toString(), Toast.LENGTH_LONG).show();
-
+                        Toast.makeText(MainActivity.this, "Some error found...", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
                     }
-                })
-                {
-                    @Nullable
+                }) {
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
-                        Map <String,String> map = new HashMap<String, String>();
-                        map.put("Username", User.getText().toString());
-                        map.put("Password", Pass.getText().toString());
-                        return map;
+                        Map<String, String> Params = new HashMap<>();
+                        Params.put("name", name);
+                        Params.put("email", email);
+                        return Params;
                     }
                 };
-                req.add(strReq);
-
+                Mysingleton.getInstance(MainActivity.this).addTorequestque(stringRequest);
             }
-
         });
     }
-
 }
---
+++ RemoteDatas/app/src/main/java/com/example/remotedatas/Mysingleton.java
@@ -0,0 +1,45 @@
+package com.example.remotedatas;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by For on 4/24/2017.
+ */
+public class Mysingleton {
+    private static Mysingleton mInstance;
+    private RequestQueue requestQueue;
+    private static Context mCtx;
+    private Mysingleton (Context Context)
+    {
+        mCtx = Context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static  synchronized Mysingleton getInstance (Context context)
+    {
+        if (mInstance==null)
+        {
+            mInstance =new Mysingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+
+        if (requestQueue==null)
+        {
+            requestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return requestQueue;
+
+    }
+
+    public<T> void addTorequestque(Request<T> request)
+    {
+        requestQueue.add(request);
+    }
+}
